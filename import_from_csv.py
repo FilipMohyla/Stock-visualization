1482226bcaefa110368d2a93223858f64461ba7d
Filip Mohyla <74453140+FilipMohyla@users.noreply.github.com>
2021-09-12 22:53:18 +0200

    Update import_from_csv.py
    I have found that somehow my previous sorting of particular columns was wrong and some lists contains more data, than they should. So I decided to change sorting process a little bit.

--- import_from_csv.py
+++ import_from_csv.py
@@ -11,24 +11,18 @@ with open("ACB.csv") as file:
     adj_close_price = []
     volume = []
 
+    row_count = 0
+
     for row in acb_csv:
 
-        for value in row:
+        date.append(row[0])
+        open_price.append(row[1])
+        high_price.append(row[2])
+        low_price.append(row[3])
+        close_price.append(row[4])
+        adj_close_price.append(row[5])
+        volume.append(row[6])
 
-            if row.index(value) == 0:
-                date.append(value)
-            if row.index(value) == 1:
-                open_price.append(value)
-            if row.index(value) == 2:
-                high_price.append(value)
-            if row.index(value) == 3:
-                low_price.append(value)
-            if row.index(value) == 4:
-                close_price.append(value)
-            if row.index(value) == 5:
-                adj_close_price.append(value)
-            if row.index(value) == 6:
-                volume.append(value)
 
-    def return_price_list():
-        return date, open_price, high_price, low_price, close_price, adj_close_price, volume
+def return_price_list():
+    return date, open_price, high_price, low_price, close_price, adj_close_price, volume
